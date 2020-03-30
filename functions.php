1abdeec0fb1705417e7f979d4832ce76261a5f24
biden warren <bidenwarren@gmail.com>
2020-03-30 01:36:14 +0000

    add function skeleton

---
+++ functions.php
@@ -0,0 +1,27 @@
+<?php 
+
+function open_conn(config[]){}
+
+//store json files
+function get_tbls($conn){}
+
+function write_tbls_list_2_json($folder,$tbls){}
+
+function write_tbl_recs_2_json($folder,$tbl){}
+
+function write_tbl_indx_2_json($folder,$tbl){} 
+
+
+
+
+//read json files
+function get_tbl_list_json($tbls_url){}
+
+function get_tbl_indx_json($tbl_indx_url){}
+
+function get_tbl_json($rec_url){}
+
+function store_json_to_db($json){}
+
+
+?>
