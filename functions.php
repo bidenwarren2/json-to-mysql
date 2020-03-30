ac8cf66bfacf4400c01def515c8ee1118120f3f7
biden warren <bidenwarren@gmail.com>
2020-03-30 13:25:36 +0000

    adding more functions

--- functions.php
+++ functions.php
@@ -2,6 +2,15 @@
 
 function open_conn(config[]){}
 
+function write_files($folder,$file_content){
+
+	//open folder for writing
+	var file = new File($folder);
+	file.write($file_content);
+	file.close();         
+}
+
+
 //store json files
 function get_tbls($conn){}
 
@@ -14,7 +23,8 @@ function write_tbls_2_json($folder,$conn){
 	 //write each table to json
          while($tbls){
 		$tbl = $tbls->field("tblname");
-		if(write_tbl_recs_2_json($folder,$tbl)){
+		$rst = "select * from ".$tbl;
+		if(write_tbl_recs_2_json($folder,$rst)){
 			$tbls_list = $tbls_list & "," & $tbl
                 } 
 		$tbls->movenext();
@@ -24,11 +34,18 @@ function write_tbls_2_json($folder,$conn){
 	 write_tbls_list_2_json($folder,$tbls_list);
 }
 
-function write_tbls_list_2_json($folder,$conn){
+function write_tbls_list_2_json($folder,$conn,$tbls){
 	 
+	//create json string
+	var $json_array = split($tbls,","); 
+	var $json = json_encode($json_array);
+	write_files($json);
 }
 
-function write_tbl_recs_2_json($folder,$tbl){}
+function write_tbl_recs_2_json($folder,$rst){
+	 
+	 //loop and write each record	
+}
 
 function write_tbl_indx_2_json($folder,$tbl){} 
 
